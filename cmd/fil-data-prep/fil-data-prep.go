fd3b0e842326113aa003cf0f1a0cc793a089569c
Anjor Kanekar <anjor@umd.edu>
2023-03-29 11:45:16 +0100

    Handle directory (#4)
    * anelace with writers
    
    * use anelace with writers
    
    * remove print
    
    * remove size for now
    
    * use with writers
    
    * this works
    
    * debugging
    
    * path -> p
    
    * semi-functioning
    
    * add carlet
    
    * this works
    
    * this works
    
    * Revert "this works"
    
    This reverts commit 719059bf2cea4e8c86e40d7e43b343eed9e21999.
    
    * fix type
    
    * refactor
    
    * remove prints
    
    * remove carlet from this branch

--- cmd/fil-data-prep/fil-data-prep.go
+++ cmd/fil-data-prep/fil-data-prep.go
@@ -2,11 +2,12 @@ package fil_data_prep
 
 import (
 	split_and_commp "data-prep/split-and-commp"
-	"data-prep/utils"
+	"encoding/json"
 	"fmt"
 	"github.com/anjor/anelace"
 	"github.com/urfave/cli/v2"
 	"io"
+	"strings"
 	"sync"
 )
 
@@ -16,75 +17,129 @@ var Cmd = &cli.Command{
 	Aliases: []string{"dp"},
 	Action:  filDataPrep,
 	Flags: []cli.Flag{
-		&cli.IntFlag{
-			Name:     "size",
-			Aliases:  []string{"s"},
-			Required: true,
-			Value:    2 << 20,
-			Usage:    "Target size in bytes to chunk CARs to.",
-		},
 		&cli.StringFlag{
 			Name:     "output",
 			Aliases:  []string{"o"},
 			Required: false,
 			Usage:    "optional output name for car files. Defaults to filename (stdin if streamed in from stdin).",
 		},
+		&cli.IntFlag{
+			Name:     "size",
+			Aliases:  []string{"s"},
+			Required: false,
+			Value:    2 << 20,
+			Usage:    "Target size in bytes to chunk CARs to.",
+		},
 		&cli.StringFlag{
 			Name:     "metadata",
 			Aliases:  []string{"m"},
 			Required: false,
-			Usage:    "optional metadata file name. Defaults to __metadata.csv",
+			Value:    "__metadata.csv",
+			Usage:    "metadata file name. ",
 		},
 	},
 }
 
 func filDataPrep(c *cli.Context) error {
-	dir, name, fi, err := utils.GetReader(c)
-	if err != nil {
-		return err
+	if !c.Args().Present() {
+		return fmt.Errorf("expected some data to be processed, found none.\n")
 	}
 
-	output := c.String("output")
-	if output != "" {
-		name = output
+	rerr, werr := io.Pipe()
+	rout, wout := io.Pipe()
+
+	anl, errs := anelace.NewAnelaceWithWriters(werr, wout)
+	if errs != nil {
+		return fmt.Errorf("unexpected error: %s\n", errs)
+	}
+	anl.SetMultipart(true)
+
+	var fileReaders []io.Reader
+	var files []string
+	paths := c.Args().Slice()
+	for _, path := range paths {
+		fs, frs, err := getAllFileReadersFromPath(path)
+		if err != nil {
+			return err
+		}
+
+		files = append(files, fs...)
+		fileReaders = append(fileReaders, frs...)
 	}
 
-	meta := c.String("metadata")
-	if meta == "" {
-		meta = "__metadata.csv"
-	}
-
-	size := c.Int("size")
-	args := []string{
-		"dolphin-songs",
-		"--emit-stderr=roots-jsonl",
-		"--emit-stdout=car-v1-stream",
-	}
-	pr, pw := io.Pipe()
 	wg := sync.WaitGroup{}
-	wg.Add(2)
-
-	anl := anelace.NewAnelaceFromArgv(args)
+	wg.Add(3)
 
 	go func() {
 		defer wg.Done()
-		defer pw.Close()
-		anl.SetCarWriter(pw)
-
-		err := anl.ProcessReader(fi, nil)
-		if err != nil {
+		defer werr.Close()
+		if err := anl.ProcessReader(io.MultiReader(fileReaders...), nil); err != nil {
 			fmt.Printf("process reader error: %s", err)
 		}
 	}()
 
+	var rs []roots
 	go func() {
 		defer wg.Done()
-		err = split_and_commp.SplitAndCommp(pr, size, meta, dir, name)
-		if err != nil {
-			fmt.Printf("errored in split and commp: %s", err)
+		defer wout.Close()
+
+		rs = getRoots(rerr)
+
+		tr := constructTree(files, rs)
+		nodes := getDirectoryNodes(tr)[1:]
+
+		var cid, sizeVi []byte
+		for _, nd := range nodes {
+			cid = []byte(nd.Cid().KeyString())
+			d := nd.RawData()
+
+			sizeVi = appendVarint(sizeVi[:0], uint64(len(cid))+uint64(len(d)))
+
+			if _, err := wout.Write(sizeVi); err == nil {
+				if _, err := wout.Write(cid); err == nil {
+					if _, err := wout.Write(d); err != nil {
+						fmt.Printf("failed to write car: %s\n", err)
+					}
+
+				}
+			}
+
+		}
+	}()
+
+	o := c.String("output")
+	m := c.String("metadata")
+	s := c.Int("size")
+
+	go func() {
+		defer wg.Done()
+
+		if err := split_and_commp.SplitAndCommp(rout, s, m, ".", o); err != nil {
+			fmt.Printf("split and commp failed: %s", err)
 		}
 	}()
 
 	wg.Wait()
+
 	return nil
+}
+
+func getRoots(rerr *io.PipeReader) []roots {
+
+	var rs []roots
+	bs, _ := io.ReadAll(rerr)
+	e := string(bs)
+	els := strings.Split(e, "\n")
+	for _, el := range els {
+		if el == "" {
+			continue
+		}
+		var r roots
+		err := json.Unmarshal([]byte(el), &r)
+		if err != nil {
+			fmt.Printf("failed to unmarshal json: %s\n", el)
+		}
+		rs = append(rs, r)
+	}
+	return rs
 }
